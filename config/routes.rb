fc236b9c679cb708e88864f749239b2f5ef3d0ff
Benjamine <benny.schmidtke@gmail.com>
2020-01-04 23:09:18 +0100

    add landing page and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'articles/index'
+  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles do
     resources :comments
   end
-  root 'articles#index'
+  root 'welcome#index'
 end
